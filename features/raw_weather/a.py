f7d0a0235207a4af8a630b7e0a0faca898891dfc
vhzkclq0705 <vhzkclq0705@naver.com>
2025-04-18 01:03:35 +0000

    csv 저장 로직 수정

--- features/raw_weather/a.py
+++ features/raw_weather/a.py
@@ -27,7 +27,7 @@ def convert_to_csv(text_data: str, save_path: str):
     df.columns = names
     df.replace(["-9", -9, "-9.0", -9.0, "-"], value=pd.NA, inplace=True)
 
-    df.to_csv(save_path, index=False)
+    df.to_csv(save_path, index=False, encoding="utf-8", line_terminator="\n")
 
 def download_weather_raw_text(ds_nodash: str, save_path: str):
     auth_key = os.getenv("WEATHER_API_KEY")
