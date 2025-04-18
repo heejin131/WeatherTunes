4eac636564fcd9ed7a7944c32d105b1a5c0beaf2
stundrg <khr0106@naver.com>
2025-04-18 14:35:16 +0900

    worker에 있는 레포를 지우자

---
+++ dags/data_songs.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=3),
+}
+
+with DAG(
+    dag_id="data_songs",
+    default_args=default_args,
+    start_date=datetime(2023, 1, 2),
+    end_date=datetime(2025, 4, 2),
+    schedule_interval="@daily",
+    catchup=True,
+    tags=["weathertunes", "audio"],
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+    
+    process_songs_data = BashOperator(
+        task_id="process_songs_data",
+        bash_command="""
+            ssh -i ~/.ssh/gcp-joon-key joon@34.47.90.224 \
+            "/home/joon/code/WeatherTunes/features/data_songs/run.sh {{ ds }} /home/joon/code/WeatherTunes/features/data_songs/b.py"
+        """
+    )
+
+    extract_audio_features = BashOperator(
+        task_id="extract_audio_features",
+        bash_command="""
+            ssh -i ~/.ssh/gcp-hyunryong-key wsl@34.64.123.456 \
+            "/home/wsl/code/WeatherTunes/features/data_songs/run.sh {{ ds }}"
+        """
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> process_songs_data  >> extract_audio_features >> end
---
+++ features/data_songs/b.py
@@ -0,0 +1,28 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_extract, col, lit
+
+def convert_to_parquet(raw_data_path: str, save_path: str, dt: str):
+    spark = SparkSession.builder.appName(f"save_song_data_{dt}").getOrCreate()
+    
+    df = spark.read.option("header", "true").csv(raw_data_path)
+    df = df.withColumn("track_id", regexp_extract("uri", r"track:([a-zA-Z0-9]+)", 1))
+    df = df.select(
+        "track_id",
+        "artist_names",
+        "track_name",
+        "days_on_cahrt",
+        "streams"
+    ).withColumn("dt", lit(dt))
+
+    df.write.mode("overwrite").partitionBy("dt").parquet(save_path)
+
+    print(f"✅ 저장 완료: {save_path} (partitioned by dt={dt})")
+
+if __name__ == "__main__":
+    ds = sys.argv[1]
+    ds_nodash = ds.replace("-", "")
+    raw_data_path = f"gs://jacob_weathertunes/raw/songs_raw/{ds}.csv"
+    save_path = f"gs://jacob_weathertunes/data/songs_data/"
+
+    convert_to_parquet(raw_data_path, save_path, ds_nodash)
