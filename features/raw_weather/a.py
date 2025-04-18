ef0fa69c208a464dd0dbd633160500810c576c7e
vhzkclq0705 <vhzkclq0705@naver.com>
2025-04-18 01:05:03 +0000

    csv 변환 및 저장 로직 수정

--- features/raw_weather/a.py
+++ features/raw_weather/a.py
@@ -23,11 +23,11 @@ def convert_to_csv(text_data: str, save_path: str):
         'BF', 'IR', 'IX'
     ]
 
-    df = pd.read_csv(StringIO(cleaned), delim_whitespace=True, header=None)
+    df = pd.read_csv(StringIO(cleaned), sep=r"\s+", header=None)
     df.columns = names
     df.replace(["-9", -9, "-9.0", -9.0, "-"], value=pd.NA, inplace=True)
 
-    df.to_csv(save_path, index=False, encoding="utf-8", line_terminator="\n")
+    df.to_csv(save_path, index=False, encoding="utf-8", lineterminator="\n")
 
 def download_weather_raw_text(ds_nodash: str, save_path: str):
     auth_key = os.getenv("WEATHER_API_KEY")
