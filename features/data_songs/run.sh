852dab872fc99c51b318a666b85b0b92f33150da
stundrg <khr0106@naver.com>
2025-04-18 16:32:47 +0900

    run.sh 수정

--- features/data_songs/run.sh
+++ features/data_songs/run.sh
@@ -7,5 +7,5 @@ PY_PATH=$2
 $SPARK_HOME/bin/spark-submit \
   --master spark://spark-1.asia-northeast3-c.c.cool-artwork-455400-v4.internal:7077 \
   --executor-memory 6G \
-  --executor-cores 4 \                                                                                                                                                                                             "$PY_PATH" "$DS"    
+  --executor-cores 4 \
   $PY_PATH $DS
