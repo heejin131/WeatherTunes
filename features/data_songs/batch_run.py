367128e18d273973358815e46e75fc3ecd5eedab
stundrg <khr0106@naver.com>
2025-04-21 23:37:00 +0900

    7/28일까지 여기까지가...

--- features/data_songs/batch_run.py
+++ features/data_songs/batch_run.py
@@ -2,7 +2,7 @@
 from datetime import datetime, timedelta
 import subprocess
 
-start_date = datetime.strptime("2023-05-12", "%Y-%m-%d")
+start_date = datetime.strptime("2023-07-27", "%Y-%m-%d")
 end_date = datetime.strptime("2024-05-31", "%Y-%m-%d")
 
 while start_date <= end_date:
