60b138f85c133c8046803075987b8179940bf82d
wminhyuk <seoseo1998@gmail.com>
2025-04-21 12:20:06 +0900

    test2

--- dags/data_weather.py
+++ dags/data_weather.py
@@ -1,33 +1,37 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
-from datetime import timedelta
+from datetime import datetime, timedelta
 import os
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+
+DAG_ID = "data_weathers"
 
 with DAG(
-    dag_id='data_weather',
+    DAG_ID,
+    default_args = {
+        'depends_on_past': False,
+        'retries': 1,
+        'retry_delay': timedelta(seconds=5),
+        },
     description='Clean raw weather data into daily summarized parquet via feature script',
-    default_args=default_args,
-    schedule_interval='@daily',
-    start_date=days_ago(1),
-    catchup=False,
-    tags=['weathertunes', 'data'],
+    schedule='@daily',
+    start_date=datetime(2023, 1, 1),
+    end_date=datetime(2025, 4, 2),
+    catchup=True,
+    max_active_runs=7,
+    concurrency=10,
+    tags=['weathertunes', 'weather', 'data'],
 ) as dag:
 
-    clean_weather = BashOperator(
-        task_id='clean_weather_data',
-        bash_command=(
-            # Airflow variable로 프로젝트 루트 지정하거나 절대경로로 작성
-            "bash {{ var.value.project_root }}/features/data_weather/run.sh {{ ds }}"
-        ),
-        env={'GCS_BUCKET': os.environ.get('GCS_BUCKET', '')},
-    )
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
 
-    clean_weather
+    clean_weather_data = BashOperator(
+        task_id="process_songs_data",
+        bash_command="""
+             ssh -i ~/.ssh/WMINHYUK_KEY SEOMINHYUK@34.64.239.242 \
+            "/home/SEOMINHYUK/code/airflow/dags/WeatherTunes/features/data_weather/run.sh {{ ds }} /home/SEOMINHYUK/code/airflow/dags/WeatherTunes/features/data_weather/a.py"
+        """
+    )
+    start >> clean_weather_data >> end
