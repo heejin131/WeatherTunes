e43dcd0530cc2f057a66b888c3e13f853cba095d
vhzkclq0705 <vhzkclq0705@naver.com>
2025-04-22 01:17:15 +0000

    Null 값 제거

--- features/meta/a.py
+++ features/meta/a.py
@@ -1,6 +1,5 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, LongType
 from pyspark.sql.functions import lit, broadcast, col
 
 def merge_data(weather_path: str, songs_path: str, audio_features_path: str, save_path: str, dt: str):
@@ -8,13 +7,6 @@ def merge_data(weather_path: str, songs_path: str, audio_features_path: str, sav
             .config("spark.sql.sources.partitionOverwriteMode", "dynamic") \
             .getOrCreate()
 
-    audio_features_schema = StructType([
-        StructField("track_id", StringType(), True),
-        StructField("BPM", LongType(), True),
-        StructField("danceability", LongType(), True),
-        StructField("happiness", LongType(), True)
-    ])
-    
     weather_df = spark.read.parquet(weather_path)
     weather_df = weather_df.withColumn("dt", lit(dt))
 
@@ -24,10 +16,7 @@ def merge_data(weather_path: str, songs_path: str, audio_features_path: str, sav
 
     audio_features_df = spark.read.schema(audio_features_schema) \
             .parquet(audio_features_path)
-    audio_features_df = audio_features_df \
-            .withColumn("BPM", col("BPM").cast("int")) \
-            .withColumn("danceability", col("danceability").cast("int")) \
-            .withColumn("happiness", col("happiness").cast("int")) \
+    audio_features_df = audio_features_df.dtopna(subset=["BPM", "danceability", "happiness"])
     
     merged_song_df = songs_df.join(broadcast(audio_features_df), on="track_id", how="left")
     merged_song_df = merged_song_df.withColumn("dt", lit(dt))
